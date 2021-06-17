948caf109af7b39d83d07b3ea20ab7d2509ee596
Hacker-Boy9999 <fordaniyal@outlook.com>
2021-06-17 12:38:10 +0300

    Update add responses.py

--- add responses.py
+++ add responses.py
@@ -1,12 +1,14 @@
+count = 0
 
-Question =input("Question: ")
-Answer =input("Answer: ")
+while count < 5:
+    Question =input("Question: ")
+    Answer =input("Answer: ")
 
-lines = ["<category>", "<pattern>", (Question), "</pattern>", (Answer), "</template>", "</category>", " "]
-with open('Responses.txt', 'a') as f:
-    for line in lines:
-        f.write(line)
-        f.write('\n')
+    lines = ["<category>", "<pattern>", (Question), "</pattern>", (Answer), "</template>", "</category>", " "]
+    with open('Responses.txt', 'a') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
     
 
 #<category>
